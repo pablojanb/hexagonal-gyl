6336fe74bc2d257f42bb6cc76ed98e61339a25da
pablojanb <pablojanb@gmail.com>
2025-06-24 10:22:26 -0300

    first commit

---
+++ src/main/java/com/example/finca_hexagonal/FincaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.finca_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finca_hexagonal/FincaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finca_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
