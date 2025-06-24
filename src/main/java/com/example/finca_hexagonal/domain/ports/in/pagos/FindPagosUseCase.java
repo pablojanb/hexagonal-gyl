a1f21bcfe6ac03c8cf1fa0c94cb0a5ee2e71f2af
Sebastian Lombo <slombo@gylgroup.local>
2025-06-24 17:11:20 -0300

    pago usecase

--- src/main/java/com/example/finca_hexagonal/domain/ports/in/pagos/FindPagosUseCase.java
+++ src/main/java/com/example/finca_hexagonal/domain/ports/in/pagos/FindPagosUseCase.java
@@ -4,7 +4,13 @@ import com.example.finca_hexagonal.domain.models.Pago;
 import org.springframework.data.domain.Page;
 
 import java.awt.print.Pageable;
+import java.util.Optional;
 
 public interface FindPagosUseCase {
     Page<Pago> getAll(Pageable pageable);
+
+    Optional<Pago> getById(Integer id);
+
+   Page<Pago> filterPagoByParams(Pageable pageable,
+                                 Boolean estado_pago,)
 }
