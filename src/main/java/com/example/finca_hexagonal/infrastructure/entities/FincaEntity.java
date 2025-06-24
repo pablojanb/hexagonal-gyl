b7297f4925ca2dde6a646051e5529a9dcc5efad1
pablojanb <pablojanb@gmail.com>
2025-06-24 11:52:58 -0300

    add lombok

--- src/main/java/com/example/finca_hexagonal/infrastructure/entities/FincaEntity.java
+++ src/main/java/com/example/finca_hexagonal/infrastructure/entities/FincaEntity.java
@@ -1,4 +1,30 @@
 package com.example.finca_hexagonal.infrastructure.entities;
 
+import com.example.finca_hexagonal.domain.models.Detalle;
+import com.example.finca_hexagonal.domain.models.Direccion;
+import com.example.finca_hexagonal.domain.models.Propietario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
 public class FincaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    private String nombre;
+    @OneToOne
+    private Propietario propietario;
+    @OneToOne
+    private Detalle detalle;
+    @OneToOne
+    private Direccion direccion;
+    private BigDecimal tarifa_hora;
 }
