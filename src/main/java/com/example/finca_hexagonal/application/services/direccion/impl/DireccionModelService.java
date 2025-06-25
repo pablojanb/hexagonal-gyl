114cd84cb2cad312deb6b664756f47d166f72262
mlbayget <marcosbayget@hotmail.com>
2025-06-25 16:55:35 -0300

    cambios 25/6

---
+++ src/main/java/com/example/finca_hexagonal/application/services/direccion/impl/DireccionModelService.java
@@ -0,0 +1,4 @@
+package com.example.finca_hexagonal.application.services.direccion.impl;
+
+public class DireccionModelServiceImpl {
+}
---
+++ src/main/java/com/example/finca_hexagonal/application/services/fecha_especial/impl/Fecha_EspecialModelService.java
@@ -0,0 +1,4 @@
+package com.example.finca_hexagonal.application.services.fecha_especial.impl;
+
+public class Fecha_EspecialModelServiceImpl {
+}
